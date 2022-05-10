63cafb0251dfbcedc3d88b9fec6109ad293c760e
Alexandre-Eteot <92881419+Alexandre77621@users.noreply.github.com>
2022-05-10 11:52:36 -0300

    aula 10.05
    aula

---
+++ 10.05/js/script.js
@@ -0,0 +1,9 @@
+function calcularPedido(){
+    //Entrada
+    Sanduiches = document.getElementById('Sanduiches')
+    Refrigerantes = document.getElementById('Refrigerantes')
+    Batatas = document.getElementById('Batatas')
+    quantSanduiches = document.getElementById('quantSanduiches')
+    quantRefrigerantes = document.getElementById('quantRefrigerantes')
+    quantBatatas = document.getElementById('quantBatatas')
+}
